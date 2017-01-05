1e809b73d58c9e660f6304a804abd4ef17c09f8a
sirwilliamiv <wjconnolly2@gmail.com>
2017-01-05 11:20:59 -0600

    added ajax call to randomuser.me and set up promise to return data

--- myJavascript.js
+++ myJavascript.js
@@ -1,36 +1,30 @@
-//sample data modified from randomUser.me
-var randomUser = {
-  "results": [
-    {
-      "gender": "female",
-      "name": {
-        "title": "ms",
-        "first": "stella",
-        "last": "meyer"
-      },
-      "location": {
-        "street": "7385 kapellenweg",
-        "city": "schmalkalden-meiningen",
-        "state": "saarland",
-        "postcode": 58225
-      },
-      "email": "stella.meyer@example.com",
-      "picture": {
-        "medium": "https:randomuser.me/api/portraits/med/women/50.jpg"
-      },
-      "nat": "DE"
-    }
-  ]
-}
+// //sample data modified from randomUser.me
+// var randomUser = {
+//   "results": [
+//     {
+//       "gender": "female",
+//       "name": {
+//         "title": "ms",
+//         "first": "stella",
+//         "last": "meyer"
+//       },
+//       "location": {
+//         "street": "7385 kapellenweg",
+//         "city": "schmalkalden-meiningen",
+//         "state": "saarland",
+//         "postcode": 58225
+//       },
+//       "email": "stella.meyer@example.com",
+//       "picture": {
+//         "medium": "https:randomuser.me/api/portraits/med/women/50.jpg"
+//       },
+//       "nat": "DE"
+//     }
+//   ]
+// }
 console.log("stuff")
 
-// JS file>
-// assign html to var
-//     var entryHTML = $("#use_this").html();
-//     var entryTemplate = Handlebars.compile(entryHTML)
-//     var entryOutput = entryTemplate(use_this) <--this is the html markup
-
-//     $("#use_this").append(entryOutput);
+function waitForTheStuf() {
 
 //handlebars step one: grap the html from the script tag
 var userHTML = $("#theUser").html()
@@ -39,7 +33,27 @@ var userHTML = $("#theUser").html()
 var userTemplate = Handlebars.compile(userHTML)
 
 //handlebars step three:render the HTML by passing the data to the template
-var userOutput = userTemplate(randomUser)
+var userOutput = userTemplate(boostUser)
 
 //handlebars step four: put the complete HTML into the DOM
 $("#putstuffhere").append(userOutput)
+
+}
+var boostUser = new Promise(function(resolve, reject){
+    $.ajax({
+      url: 'https://randomuser.me/api/',
+      dataType: 'json',
+      success: function(data) {
+        resolve(data)
+        // console.log(data);
+      }
+    });
+})
+
+  boostUser
+    .then(
+  function(data){
+    boostUser = data
+    // console.log("hey", boostUser)
+    waitForTheStuf()
+  })
